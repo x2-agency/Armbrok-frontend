cc1571e8a33e921d6353041a77391797bc7d1094
evgeniy.gilyazetdinov <jhongil3007@gmail.com>
2025-02-01 20:20:12 +0300

    feat: добавить postcss + config

---
+++ argentina-brok/postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	plugins: {
+		autoprefixer: {},
+		'postcss-mixins': {
+			mixinsDir: '',
+		},
+		'postcss-simple-vars': {
+			variables: {},
+		},
+		'postcss-import': {
+			path: [''],
+		},
+	},
+};
