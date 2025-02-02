4a58405a0ef4e217df30f45c25f61f0323b78d21
evgeniy.gilyazetdinov <jhongil3007@gmail.com>
2025-02-02 18:29:11 +0300

    feat: delete not usable

--- argentina-brok/widgets/banner-section/ui/banner-head/index.tsx
+++ argentina-brok/widgets/banner-section/ui/banner-head/index.tsx
@@ -6,7 +6,6 @@ import type { BannerHeadProps } from '@/widgets/banner/banner.types';
 import { BackButton } from '../back-button';
 
 import css from './index.module.css';
-import { Container } from '@/shared/ui/container';
 
 export const BannerHead = ({
 	title,
@@ -17,7 +16,7 @@ export const BannerHead = ({
 	withBackButton,
 }: BannerHeadProps) => {
 	return (
-		<Container className={cx(css.root, className)}>
+		<section className={cx(css.root, className)}>
 			{withBackButton && <BackButton>Back</BackButton>}
 			{established && (
 				<div className={css.established}>{parser(established)}</div>
@@ -25,6 +24,6 @@ export const BannerHead = ({
 			<h1 className={css.title}>{parser(title)}</h1>
 			<p className={css.description}>{parser(description)}</p>
 			{icon && <img src={icon} alt="Icon" />}
-		</Container>
+		</section>
 	);
 };
--- argentina-brok/widgets/banner-section/ui/index.tsx
+++ argentina-brok/widgets/banner-section/ui/index.tsx
@@ -6,6 +6,7 @@ import { BannerBody } from './banner-body';
 import { BannerHead } from './banner-head';
 import css from './index.module.css';
 import { FormatImage } from '@/shared/ui/format-image';
+import { Container } from '@/shared/ui/container';
 
 export const BannerSection = ({
 	alignContent = 'center',
@@ -14,12 +15,15 @@ export const BannerSection = ({
 }: BannerProps) => {
 	return (
 		<section className={cx(css.root, css[size])} data-align={alignContent}>
-			<BannerHead title={banner.title} description={banner.description} />
-			<BannerBody
-				awards={banner.awards}
-				panel={banner.panel}
-				button={banner.button}
-			/>
+			<Container className={css.content}>
+				<BannerHead title={banner.title} description={banner.description} />
+				<BannerBody
+					awards={banner.awards}
+					panel={banner.panel}
+					button={banner.button}
+				/>
+			</Container>
+
 			{banner.poster && (
 				<FormatImage poster={banner.poster} className={css.poster} />
 			)}
