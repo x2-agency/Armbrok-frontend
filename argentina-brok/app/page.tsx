70f9cad66bd9a1029b11d285f203a1eae58d89d9
evgeniy.gilyazetdinov <jhongil3007@gmail.com>
2025-02-08 17:19:01 +0300

    feat: применить дефолтный баннер

--- argentina-brok/app/page.tsx
+++ argentina-brok/app/page.tsx
@@ -6,7 +6,13 @@ import {
 import { BannerSection } from '@/widgets/banner-section/ui';
 
 const HomePage = () => {
-	return <BannerSection type="about" banner={MOCK_ABOUT_BANNER} />;
+	return (
+		<BannerSection
+			type="default"
+			banner={MOCK_DEFAULT_BANNER}
+			alignContent="end"
+		/>
+	);
 };
 
 export default HomePage;
