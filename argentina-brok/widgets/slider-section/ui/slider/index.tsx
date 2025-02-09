47e4d86a075431e77b8dc81681349603f971b1df
evgeniy.gilyazetdinov <jhongil3007@gmail.com>
2025-02-09 19:47:08 +0300

    feat: стилизовать свайпер

--- argentina-brok/widgets/slider-section/ui/slider/index.tsx
+++ argentina-brok/widgets/slider-section/ui/slider/index.tsx
@@ -1,7 +1,12 @@
 /* eslint-disable @next/next/no-img-element */
 'use client';
-import 'swiper/css';
+import { EffectCoverflow, Navigation, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import parser from 'html-react-parser';
 
 import type { SliderSectionProps } from '@/widgets/slider-section/slider-section.types';
 
@@ -14,16 +19,27 @@ export const Slider = ({
 }) => {
 	return (
 		<Swiper
+			effect="coverflow"
 			slidesPerView="auto"
 			className={css.swiper}
 			centeredSlides={true}
 			spaceBetween={50}
 			slideToClickedSlide
+			slideActiveClass={css.activeSlide}
 			setWrapperSize={false}
+			coverflowEffect={{
+				slideShadows: false,
+				depth: 100,
+				scale: 1,
+				rotate: 0,
+			}}
+			modules={[EffectCoverflow, Navigation, Pagination]}
 		>
 			{slider.map((value, index) => (
 				<SwiperSlide key={index} className={css.slide}>
 					<img src={value.image} alt="image" className={css.image} />
+					<h4 className={css.title}>{parser(value.title)}</h4>
+					<p className={css.description}>{parser(value.description)}</p>
 				</SwiperSlide>
 			))}
 		</Swiper>
