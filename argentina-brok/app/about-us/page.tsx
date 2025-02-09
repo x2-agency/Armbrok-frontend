11f183bf02f62a6a98a4057803343f5819d3cf94
evgeniy.gilyazetdinov <jhongil3007@gmail.com>
2025-02-09 20:30:25 +0300

    feat: добавить мету

--- argentina-brok/app/about-us/page.tsx
+++ argentina-brok/app/about-us/page.tsx
@@ -1,4 +1,10 @@
+import type { Metadata } from 'next';
+
 import { AboutUs } from '@/view/about-us';
+
+export const metadata: Metadata = {
+	title: 'About us',
+};
 
 const AboutUsPage = () => {
 	return <AboutUs />;
--- argentina-brok/app/investment-banking/page.tsx
+++ argentina-brok/app/investment-banking/page.tsx
@@ -1,4 +1,10 @@
+import type { Metadata } from 'next';
+
 import { InvestmentBanking } from '@/view/investment-banking';
+
+export const metadata: Metadata = {
+	title: 'Investment Banking',
+};
 
 const InvestmentBankingPage = () => {
 	return <InvestmentBanking />;
