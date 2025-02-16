2e310db68abaa726e4a758ec4c3a2ba01df80e17
evgeniy.gilyazetdinov <jhongil3007@gmail.com>
2025-02-17 01:01:22 +0300

    feat: добавить модально окно кукки

--- argentina-brok/widgets/app-layout/index.tsx
+++ argentina-brok/widgets/app-layout/index.tsx
@@ -1,18 +1,25 @@
 'use client';
 
+import { usePathname } from 'next/navigation';
 import type { PropsWithChildren } from 'react';
 
 import { headerScrollObserver } from '@/shared/lib/header-scroll-observer';
 
+import { SHOW_PANEL_URLS } from './models/cookie-panel.constants';
+import { CookiePanel } from './ui/cookie-panel';
 import { Footer } from './ui/footer';
 import { Header } from './ui/header';
 import { Menu } from './ui/menu';
 
 export const AppLayout = ({ children }: Readonly<PropsWithChildren>) => {
+	const router = usePathname();
+	const isShowPanel = SHOW_PANEL_URLS.includes(router);
+
 	return (
 		<headerScrollObserver.ScrollObserverProvider>
 			<Header />
 			<Menu />
+			{isShowPanel && <CookiePanel />}
 			<main>{children}</main>
 			<Footer />
 		</headerScrollObserver.ScrollObserverProvider>
