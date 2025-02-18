5a76ca6b410d3f2cfe0c726c6cf15d262a42da13
Nikita Slesarev <nikita.slesarev@chulakov.com>
2025-02-19 01:47:40 +0300

    refactor: настроить favicon

--- argentina-brok/app/layout.tsx
+++ argentina-brok/app/layout.tsx
@@ -3,17 +3,21 @@ import type { PropsWithChildren } from 'react';
 import { RobotoFonts } from '@/shared/config/localFont';
 import { LayoutProvider } from '@/shared/hooks/use-layout-context';
 import '@/shared/styles/mixins/mixin.css';
+import { Favicons } from '@/view/documents/ui/favicons';
 import { AppLayout } from '@/widgets/app-layout';
 
-import './global.css';
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import './global.css';
 
 const RootLayout = ({ children }: Readonly<PropsWithChildren>) => {
 	return (
 		<html lang="en">
+			<head>
+				<Favicons />
+			</head>
 			<body className={RobotoFonts.className}>
 				<LayoutProvider>
 					<AppLayout>{children}</AppLayout>
