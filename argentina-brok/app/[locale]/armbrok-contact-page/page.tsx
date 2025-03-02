8d49157de6a82a0d3d56e5f5bdf8ca1dd1b88bf8
evgeniy.gilyazetdinov <jhongil3007@gmail.com>
2025-03-02 15:19:07 +0300

    feat: добавить мету к контактам

--- argentina-brok/app/[locale]/armbrok-contact-page/page.tsx
+++ argentina-brok/app/[locale]/armbrok-contact-page/page.tsx
@@ -1,6 +1,11 @@
+import type { Metadata } from 'next';
+
 import { getContactPage } from '@/shared/api/get-contact-page';
 import { ArmbrokContact } from '@/view/armbrok-contact';
+import { ARMBROK_CONTACT_META } from '@/view/armbrok-contact/model/armbrok-contact.meta';
 import type { ArmbrokContactPageResponse } from '@/view/armbrok-contact/types/response';
+
+export const metadata: Metadata = ARMBROK_CONTACT_META;
 
 const ArmbrokContactPage = async () => {
 	const inititalContactPageData: ArmbrokContactPageResponse =
