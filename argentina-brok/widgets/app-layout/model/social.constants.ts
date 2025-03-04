112b97f14b6810db71bbf4a1de6e63535a3fb47d
Nikita Slesarev <nikita.slesarev@chulakov.com>
2025-03-05 01:01:07 +0300

    refactor: обновить футер, коикабельные свг иконки

--- argentina-brok/widgets/app-layout/model/social.constants.ts
+++ argentina-brok/widgets/app-layout/model/social.constants.ts
@@ -1,6 +1,4 @@
-import AppStoreIcon from '@/public/assets/social/app_store.svg';
 import FacebookIcon from '@/public/assets/social/facebook.svg';
-import GooglePlayIcon from '@/public/assets/social/google_play.svg';
 import InstagramIcon from '@/public/assets/social/instagram.svg';
 import LinkedInIcon from '@/public/assets/social/linkedin.svg';
 import TelegramIcon from '@/public/assets/social/telegram.svg';
@@ -16,19 +14,6 @@ export type IconListProps = {
 	items: Array<Icon>;
 	className?: string;
 };
-
-export const STORE_LINKS: Array<Icon> = [
-	{
-		width: 100,
-		href: 'https://play.google.com/store/games',
-		SvgIcon: GooglePlayIcon,
-	},
-	{
-		width: 100,
-		href: 'https://www.apple.com/app-store',
-		SvgIcon: AppStoreIcon,
-	},
-];
 
 export const SOCIAL_MEDIA_LINKS: Array<Icon> = [
 	{
--- argentina-brok/widgets/app-layout/ui/footer/social-links/index.tsx
+++ argentina-brok/widgets/app-layout/ui/footer/social-links/index.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import AppSvg from '@/public/assets/social/app_store.svg';
 import GooglePlaySvg from '@/public/assets/social/google_play.svg';
 import { SOCIAL_MEDIA_LINKS } from '@/widgets/app-layout/model/social.constants';
@@ -9,8 +11,20 @@ export const SocialLinks = () => {
 	return (
 		<div className={css.root}>
 			<div className={css.appStore}>
-				<AppSvg className={css.svg} />
-				<GooglePlaySvg className={css.svg} />
+				<Link
+					href="https://apps.apple.com"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<AppSvg className={css.svg} />
+				</Link>
+				<Link
+					href="https://play.google.com"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<GooglePlaySvg className={css.svg} />
+				</Link>
 			</div>
 			<Social className={css.media} items={SOCIAL_MEDIA_LINKS} />
 		</div>
