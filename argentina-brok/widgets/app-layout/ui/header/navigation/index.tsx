a1c3d252bbd0510b8b249a38558a380f638224ff
Nikita Slesarev <nikita.slesarev@chulakov.com>
2025-03-06 01:35:02 +0300

    fix: хедер на армянском

--- argentina-brok/widgets/app-layout/ui/header/navigation/index.tsx
+++ argentina-brok/widgets/app-layout/ui/header/navigation/index.tsx
@@ -1,4 +1,6 @@
 import cx from 'clsx';
+import parser from 'html-react-parser';
+import { usePathname } from 'next/navigation';
 
 import { Button } from '@/shared/ui/button';
 import { useHeaderLinks } from '@/widgets/app-layout/hooks/use-header-links';
@@ -6,15 +8,17 @@ import { useHeaderLinks } from '@/widgets/app-layout/hooks/use-header-links';
 import css from './index.module.css';
 
 export const Navigation = () => {
+	const pathname = usePathname();
+	const isHyLocale = pathname.startsWith('/hy');
 	const links = useHeaderLinks();
 
 	return (
 		<nav className={cx(css.root)}>
-			<ul className={css.links}>
+			<ul className={cx(css.links, { [css.linksHy]: isHyLocale })}>
 				{links.map((item, index) => (
-					<li key={index}>
+					<li className={css.li} key={index}>
 						<Button href={item.href} className={css.link} variant="subtle">
-							{item.label}
+							{parser(item.label)}
 						</Button>
 					</li>
 				))}
