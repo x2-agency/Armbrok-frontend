76aa5b9fe960a4abd333e616ab270d20211661f5
evgeniy.gilyazetdinov <jhongil3007@gmail.com>
2025-03-15 19:03:14 +0300

    feat: сделать локализацию модалки

--- argentina-brok/widgets/app-layout/ui/account-modal/account-form/index.tsx
+++ argentina-brok/widgets/app-layout/ui/account-modal/account-form/index.tsx
@@ -2,6 +2,7 @@
 'use client';
 
 import parser from 'html-react-parser';
+import { useTranslations } from 'next-intl';
 import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 
@@ -11,6 +12,7 @@ import { Captcha } from '@/shared/ui/captcha';
 import { Checkbox } from '@/shared/ui/checkbox';
 import { Input } from '@/shared/ui/input';
 import { Textarea } from '@/shared/ui/textarea';
+import { useAccountTranslations } from '@/widgets/app-layout/hooks/use-account-translations';
 import { ACCOUNT } from '@/widgets/app-layout/model/account-form.constants';
 import {
 	HOME_LINK,
@@ -33,6 +35,17 @@ type FormValues = {
 export const AccountForm = () => {
 	const [isCaptchaChecked, toggleCaptcha] = useState<boolean>(false);
 	const [isSuccess, toggleSuccess] = useState<boolean>(false);
+	const {
+		nameInputTranslation,
+		messageInputTranslation,
+		subjectInputTranslation,
+		phoneInputTranslation,
+		emailInputTranslation,
+		checkboxTranslation,
+		captchaTranslation,
+		buttonTranslation,
+		titleTranslation,
+	} = useAccountTranslations();
 
 	const {
 		formState: { isValid, errors },
@@ -56,17 +69,14 @@ export const AccountForm = () => {
 		telInput,
 		emailInput,
 		messageTextArea,
-		captcha,
-		checkbox,
 		title,
-		button,
 		subjectInput,
 	} = ACCOUNT;
 
 	return (
 		<div className={css.root}>
 			<Logo logo={LOGO_HEADER} href={HOME_LINK} className={css.logo} />
-			<h3 className={css.title}>{parser(title)}</h3>
+			<h3 className={css.title}>{parser(titleTranslation)}</h3>
 			{isSuccess ? (
 				<SuccessForm />
 			) : (
@@ -78,103 +88,106 @@ export const AccountForm = () => {
 						<Input
 							className={css.input}
 							type={nameInput.type}
-							placeholder={nameInput.placeholder}
-							label={nameInput.label}
+							placeholder={nameInputTranslation.placeholder}
+							label={nameInputTranslation.label}
 							required={nameInput.required}
 							{...register('name', {
 								required: nameInput.required,
 								minLength: {
 									value: 4,
-									message: 'Name must be at least 4 characters long',
+									message: nameInputTranslation.secondErrorMessage,
 								},
 							})}
 							aria-invalid={Boolean(errors.name)}
-							aria-errormessage={errors.name?.message || nameInput.errorMessage}
+							aria-errormessage={
+								errors.name?.message || nameInputTranslation.errorMessage
+							}
 						/>
 						<Input
 							className={css.input}
 							type={telInput.type}
-							placeholder={telInput.placeholder}
-							label={telInput.label}
+							placeholder={phoneInputTranslation.placeholder}
+							label={phoneInputTranslation.label}
 							required={telInput.required}
 							{...register('phoneNumber', {
 								required: telInput.required,
 								pattern: {
 									value:
 										/^\+?[0-9]{1,4}?[-.\s]?(\([0-9]{1,4}\)|[0-9]{1,4})[-.\s]?[0-9]{1,4}[-.\s]?[0-9]{1,9}$/,
-									message: 'Please enter a valid phone number',
+									message: phoneInputTranslation.secondErrorMessage,
 								},
 							})}
 							aria-invalid={Boolean(errors.phoneNumber)}
 							aria-errormessage={
-								errors.phoneNumber?.message || telInput.errorMessage
+								errors.phoneNumber?.message ||
+								phoneInputTranslation.errorMessage
 							}
 						/>
 						<Input
 							className={css.input}
 							type="email"
-							placeholder={emailInput.placeholder}
-							label={emailInput.label}
+							placeholder={emailInputTranslation.placeholder}
+							label={emailInputTranslation.label}
 							required={emailInput.required}
 							{...register('email', {
-								required: emailInput.required ? 'Email is required' : false,
+								required: emailInput.required,
 								pattern: {
 									value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-									message: 'Please enter a valid email address',
+									message: emailInputTranslation.secondErrorMessage,
 								},
 							})}
 							aria-invalid={Boolean(errors.email)}
 							aria-errormessage={
-								errors.email?.message || emailInput.errorMessage
+								errors.email?.message || emailInputTranslation.errorMessage
 							}
 						/>
 						<Input
 							className={css.input}
 							type={subjectInput.type}
-							placeholder={subjectInput.placeholder}
-							label={subjectInput.label}
+							placeholder={subjectInputTranslation.placeholder}
+							label={subjectInputTranslation.label}
 							required={subjectInput.required}
 							value={'Open account'}
 							{...register('subject', {
 								required: nameInput.required,
 								minLength: {
 									value: 4,
-									message: 'Subject must be at least 4 characters long',
+									message: subjectInputTranslation.secondErrorMessage,
 								},
 							})}
 							readOnly
 							aria-invalid={Boolean(errors.subject)}
 							aria-errormessage={
-								errors.name?.message || subjectInput.errorMessage
+								errors.name?.message || subjectInputTranslation.errorMessage
 							}
 						/>
 					</div>
 					<Textarea
 						rows={3}
 						className={css.textarea}
-						placeholder={messageTextArea.placeholder}
-						label={messageTextArea.label}
+						placeholder={messageInputTranslation.placeholder}
+						label={messageInputTranslation.label}
 						required={messageTextArea.required}
 						{...register('message', {
 							required: messageTextArea.required,
 							minLength: {
 								value: 10,
-								message: 'Message must be at least 10 characters long',
+								message: messageInputTranslation.secondErrorMessage,
 							},
 						})}
 						aria-invalid={Boolean(errors.message)}
 						aria-errormessage={
-							errors.message?.message || messageTextArea.errorMessage
+							errors.message?.message || messageInputTranslation.errorMessage
 						}
 					/>
 					<Checkbox
-						label={checkbox.label}
+						label={checkboxTranslation}
 						required={true}
 						className={css.checkbox}
 						{...register('checkbox')}
 					/>
 					<Captcha
-						subtitle={captcha.text}
+						subtitle={captchaTranslation}
 						onChange={() => toggleCaptcha(true)}
 						className={css.captcha}
 					/>
@@ -185,7 +198,7 @@ export const AccountForm = () => {
 						category="big"
 						className={css.button}
 					>
-						{parser(button.text)}
+						{parser(buttonTranslation)}
 					</Button>
 				</form>
 			)}
