bef91c58955e3b189bc09209ef6c1f0674681969
Nikita Slesarev <nikita.slesarev@chulakov.com>
2025-03-16 03:23:03 +0300

    fix: сделал аккардион полностью кликабельным

--- argentina-brok/widgets/helpful-information/ui/accordion/index.tsx
+++ argentina-brok/widgets/helpful-information/ui/accordion/index.tsx
@@ -1,4 +1,3 @@
-import cx from 'clsx';
 import parser from 'html-react-parser';
 
 import AccordionArrowSVG from '@/public/assets/icons/accordion-arrow.svg';
@@ -8,7 +7,7 @@ import css from './index.module.css';
 
 export const Accordion = ({ summary, description }: AccordionProps) => {
 	return (
-		<article className={cx(css.root, 'p-32')}>
+		<article className={css.root}>
 			<details className={css.details}>
 				<summary className={css.summary}>
 					<h4 className={css.title}>{parser(summary)}</h4>
