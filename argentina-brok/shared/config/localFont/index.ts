cdd315bdbaf74642298c9e9d603d8d0b8085adf4
evgeniy.gilyazetdinov <jhongil3007@gmail.com>
2025-03-23 15:22:28 +0300

    feat: переименовать шрифты

--- argentina-brok/shared/config/localFont/index.ts
+++ argentina-brok/shared/config/localFont/index.ts
@@ -3,22 +3,22 @@ import localFont from 'next/font/local';
 export const RobotoFonts = localFont({
 	src: [
 		{
-			path: '../../../public/fonts/Roboto-Regular.woff2',
+			path: '../../../public/fonts/roboto-regular.woff2',
 			weight: '400',
 			style: 'normal',
 		},
 		{
-			path: '../../../public/fonts/Roboto-Medium.woff2',
+			path: '../../../public/fonts/roboto-medium.woff2',
 			weight: '500',
 			style: 'normal',
 		},
 		{
-			path: '../../../public/fonts/Roboto-SemiBold.woff2',
+			path: '../../../public/fonts/roboto-semibold.woff2',
 			weight: '600',
 			style: 'normal',
 		},
 		{
-			path: '../../../public/fonts/Roboto-Bold.woff2',
+			path: '../../../public/fonts/roboto-bold.woff2',
 			weight: '700',
 			style: 'normal',
 		},
