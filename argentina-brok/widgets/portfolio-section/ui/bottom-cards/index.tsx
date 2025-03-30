ac5687ebf8dd5d35793cc43cbf6917905f7a5b2d
evgeniy.gilyazetdinov <jhongil3007@gmail.com>
2025-03-30 21:20:36 +0300

    feat: применить правки по слайдеру

--- argentina-brok/widgets/portfolio-section/ui/bottom-cards/index.tsx
+++ argentina-brok/widgets/portfolio-section/ui/bottom-cards/index.tsx
@@ -1,3 +1,9 @@
+'use client';
+
+import { Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import useMediaQuery from '@/shared/hooks/use-media-query';
 import type { PortfolioSectionType } from '@/view/brokerage/types/response';
 
 import { Card } from './card';
@@ -8,6 +14,38 @@ export type BottomCardsProps = {
 };
 
 export const BottomCards = ({ cards }: BottomCardsProps) => {
+	const isMobile = useMediaQuery('(max-width: 767px)');
+
+	if (isMobile) {
+		return (
+			<div className={css.sliderWrapper}>
+				<Swiper
+					pagination
+					modules={[Pagination]}
+					spaceBetween={20}
+					centeredSlides
+					autoHeight={false} 
+					slidesPerView={1}
+					style={{ height: '100%' }} 
+					className={css.swiper}
+				>
+					{cards.map(card => (
+						<SwiperSlide key={card.id} className={css.slide}>
+							<Card
+								className={css.card}
+								data={{
+									title: card?.title,
+									description: card?.description,
+									mediaContent: card?.mediaContent,
+								}}
+							/>
+						</SwiperSlide>
+					))}
+				</Swiper>
+			</div>
+		);
+	}
+
 	return (
 		<div className={css.root}>
 			{cards.map(card => (
