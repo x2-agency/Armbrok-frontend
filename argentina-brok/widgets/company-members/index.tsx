d778e53ae9cd246e21c38973118b451a70e05a23
evgeniy.gilyazetdinov <jhongil3007@gmail.com>
2025-04-03 01:20:31 +0300

    feat: интегрировать дерево

--- argentina-brok/widgets/company-members/index.tsx
+++ argentina-brok/widgets/company-members/index.tsx
@@ -6,10 +6,12 @@ import useMediaQuery from '@/shared/hooks/use-media-query';
 import type {
 	CompanyStructureSection,
 	EmployeeList,
+	TreeItem,
 } from '@/shared/types/global.types';
 import { Container } from '@/shared/ui/container';
 import { Tabs } from '@/widgets/company-members/ui/tabs';
 
+import { sortTreeLevels } from './helpers/sort-tree-levels';
 import css from './index.module.css';
 import { CompanyMembersSection } from './ui/members-section';
 import { TreeDesktop } from './ui/tree-desktop';
@@ -22,29 +24,39 @@ export const CompanyMembers = ({ tabs, title }: CompanyStructureSection) => {
 		return null;
 	}
 
-	const tabsElem = tabs
-		.map(tab => {
-			const employeesList = tab.content.find(item => 'employees' in item) as
-				| EmployeeList
-				| undefined;
+	const tabsElem = tabs.map(tab => {
+		const employeesList = tab.content.find(
+			item => item.__component === 'shared.employee-list'
+		) as EmployeeList | undefined;
 
-			const isTeamTab = ['Team', 'Команда', 'Թիմ'].includes(tab.tabName);
+		const departmentsList = tab.content.filter(
+			item => item.__component === 'shared.company-department'
+		) as Array<TreeItem>;
 
+		if (employeesList) {
 			return {
 				label: tab.tabName,
-				content: isTeamTab ? (
-					employeesList ? (
-						<CompanyMembersSection employees={employeesList.employees} />
-					) : null
-				) : isMobile ? (
-					<TreeMobile />
-				) : (
-					<TreeDesktop />
-				),
-				isTeamTab,
+				content: <CompanyMembersSection employees={employeesList.employees} />,
 			};
-		})
-		.sort(a => (a.isTeamTab ? -1 : 1));
+		}
+
+		if (departmentsList.length > 0) {
+			const treeSortedContent = sortTreeLevels(departmentsList);
+			return {
+				label: tab.tabName,
+				content: isMobile ? (
+					<TreeMobile {...treeSortedContent} />
+				) : (
+					<TreeDesktop {...treeSortedContent} />
+				),
+			};
+		}
+
+		return {
+			label: tab.tabName,
+			content: null,
+		};
+	});
 
 	return (
 		<Container className={css.root}>
--- argentina-brok/widgets/company-members/ui/tree-mobile/index.tsx
+++ argentina-brok/widgets/company-members/ui/tree-mobile/index.tsx
@@ -1,85 +1,74 @@
 import cx from 'clsx';
+import parser from 'html-react-parser';
+
+import type { TreeItem } from '@/shared/types/global.types';
 
 import css from './index.module.css';
 
-export const TreeMobile = () => {
+export const TreeMobile = (content: Record<number, Array<TreeItem>>) => {
 	return (
 		<article className={css.root}>
-			<div className={css.topElement}>General Meeting of Shareholders</div>
+			<div className={css.topElement}>{parser(content[1][0].value ?? '')}</div>
 			<div className={css.topElement}>
 				<div className={css.circleTop} />
-				Chief Executive Officer
+				{parser(content[2][0].value ?? '')}
 			</div>
 			<div className={css.body}>
 				<div className={css.left}>
 					<div className={css.element}>
 						<div className={css.circleTop} />
-						Investment
-						<br /> banking
+						{parser(content[3][0].value ?? '')}
 					</div>
 					<div className={css.element}>
 						<div className={css.circleTop} />
-						Asset
-						<br /> management
+						{parser(content[3][1].value ?? '')}
 					</div>
 					<div className={cx(css.element, css.leftArrow)}>
 						<div className={css.circleTop} />
 						<div className={css.circleLeft} />
-						Securities
-						<br />
-						trading
+						{parser(content[3][2].value ?? '')}
 					</div>
 					<div className={css.element}>
 						<div className={css.circleTop} />
-						Consulting
+						{parser(content[3][3].value ?? '')}
 					</div>
 					<div className={css.element}>
 						<div className={css.circleTop} />
-						International
-						<br />
-						projects
+						{parser(content[3][4].value ?? '')}
 					</div>
 					<div className={css.element}>
 						<div className={css.circleTop} />
-						Research
-						<br />& analysis
+						{parser(content[3][5].value ?? '')}
 					</div>
 					<div className={cx(css.element, css.leftArrow)}>
 						<div className={css.circleTop} />
 						<div className={css.circleLeft} />
-						Depositary
-						<br /> services
+						{parser(content[3][6].value ?? '')}
 					</div>
 					<div className={css.element}>
 						<div className={css.circleTop} />
-						Accounting
+						{parser(content[3][7].value ?? '')}
 					</div>
 					<div className={css.element}>
 						<div className={css.circleTop} />
-						Internal
-						<br />
-						audit
+						{parser(content[3][8].value ?? '')}
 					</div>
 				</div>
 				<div className={css.right}>
 					<div className={css.rightGroup}>
 						<div className={css.rightElement}>
-							Global
-							<br /> market
+							{parser(content[5][1].value ?? '')}
 						</div>
 						<div className={css.rightElement}>
-							Armenian
-							<br /> market
+							{parser(content[5][0].value ?? '')}
 						</div>
 					</div>
 					<div className={css.rightGroup}>
 						<div className={css.rightElement}>
-							Custody
-							<br /> services
+							{parser(content[4][1].value ?? '')}
 						</div>
 						<div className={css.rightElement}>
-							Registry
-							<br /> keeping
+							{parser(content[4][0].value ?? '')}
 						</div>
 					</div>
 				</div>
