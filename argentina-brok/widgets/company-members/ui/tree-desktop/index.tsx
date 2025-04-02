aae46b79722c48640fa4f8ce2466e17aaa2dba32
evgeniy.gilyazetdinov <jhongil3007@gmail.com>
2025-04-03 01:39:34 +0300

    feat: настроить стили для дерева

--- argentina-brok/widgets/company-members/ui/tree-desktop/index.tsx
+++ argentina-brok/widgets/company-members/ui/tree-desktop/index.tsx
@@ -1,12 +1,16 @@
+'use client';
+
 import cx from 'clsx';
 import parser from 'html-react-parser';
+import { usePathname } from 'next/navigation';
 
 import type { TreeItem } from '@/shared/types/global.types';
 
 import css from './index.module.css';
 
 export const TreeDesktop = (content: Record<number, Array<TreeItem>>) => {
-	console.log(content);
+	const locale = usePathname().split('/')[1];
+
 	return (
 		<article className={css.root}>
 			<div className={css.element}>{parser(content[1][0].value)}</div>
@@ -15,63 +19,77 @@ export const TreeDesktop = (content: Record<number, Array<TreeItem>>) => {
 				{parser(content[2][0].value ?? '')}
 				<div className={css.circleBottom} />
 			</div>
-			<div className={cx(css.group, css.first)}>
-				<div className={css.elementSecondType}>
+			<div className={cx(css.group, css.first, css[locale])}>
+				<div className={cx(css.elementSecondType, css[locale])}>
 					<div className={css.circleTop} />
 					{parser(content[3][0].value ?? '')}
 				</div>
-				<div className={cx(css.elementSecondType, css.withTop)}>
+				<div className={cx(css.elementSecondType, css.withTop, css[locale])}>
 					<div className={css.circleTop} />
 					{parser(content[3][1].value ?? '')}
 				</div>
-				<div className={cx(css.elementSecondType, css.withTop, css.withBottom)}>
+				<div
+					className={cx(
+						css.elementSecondType,
+						css.withTop,
+						css.withBottom,
+						css[locale]
+					)}
+				>
 					<div className={css.circleTop} />
 					{parser(content[3][2].value ?? '')}
 					<div className={css.circleBottom} />
 				</div>
-				<div className={cx(css.elementSecondType, css.withTop)}>
+				<div className={cx(css.elementSecondType, css.withTop, css[locale])}>
 					<div className={css.circleTop} />
 					{parser(content[3][3].value ?? '')}
 				</div>
-				<div className={cx(css.elementSecondType)}>
+				<div className={cx(css.elementSecondType, css[locale])}>
 					<div className={css.circleTop} />
 					{parser(content[3][4].value ?? '')}
 				</div>
-				<div className={cx(css.elementSecondType, css.withTop)}>
+				<div className={cx(css.elementSecondType, css.withTop, css[locale])}>
 					<div className={css.circleTop} />
 					{parser(content[3][5].value ?? '')}
 				</div>
-				<div className={cx(css.elementSecondType, css.withTop, css.withBottom)}>
+				<div
+					className={cx(
+						css.elementSecondType,
+						css.withTop,
+						css.withBottom,
+						css[locale]
+					)}
+				>
 					<div className={css.circleTop} />
 					{parser(content[3][6].value ?? '')}
 					<div className={css.circleBottom} />
 				</div>
-				<div className={cx(css.elementSecondType, css.withTop)}>
+				<div className={cx(css.elementSecondType, css.withTop, css[locale])}>
 					<div className={css.circleTop} />
 					{parser(content[3][7].value ?? '')}
 				</div>
-				<div className={css.elementSecondType}>
+				<div className={cx(css.elementSecondType, css[locale])}>
 					<div className={css.circleTop} />
 					{parser(content[3][8].value ?? '')}
 				</div>
 			</div>
 			<div className={css.underLevel}>
 				<div className={cx(css.group, css.second)}>
-					<div className={css.elementThirdType}>
+					<div className={cx(css.elementThirdType, css[locale])}>
 						<div className={css.circleTop} />
 						{parser(content[4][0].value ?? '')}
 					</div>
-					<div className={css.elementThirdType}>
+					<div className={cx(css.elementThirdType, css[locale])}>
 						<div className={css.circleTop} />
 						{parser(content[4][1].value ?? '')}
 					</div>
 				</div>
 				<div className={cx(css.group, css.second)}>
-					<div className={css.elementThirdType}>
+					<div className={cx(css.elementThirdType, css[locale])}>
 						<div className={css.circleTop} />
 						{parser(content[5][0].value ?? '')}
 					</div>
-					<div className={css.elementThirdType}>
+					<div className={cx(css.elementThirdType, css[locale])}>
 						<div className={css.circleTop} />
 						{parser(content[5][1].value ?? '')}
 					</div>
--- argentina-brok/widgets/company-members/ui/tree-mobile/index.tsx
+++ argentina-brok/widgets/company-members/ui/tree-mobile/index.tsx
@@ -1,11 +1,16 @@
+'use client';
+
 import cx from 'clsx';
 import parser from 'html-react-parser';
+import { usePathname } from 'next/navigation';
 
 import type { TreeItem } from '@/shared/types/global.types';
 
 import css from './index.module.css';
 
 export const TreeMobile = (content: Record<number, Array<TreeItem>>) => {
+	const locale = usePathname().split('/')[1];
+
 	return (
 		<article className={css.root}>
 			<div className={css.topElement}>{parser(content[1][0].value ?? '')}</div>
@@ -15,59 +20,59 @@ export const TreeMobile = (content: Record<number, Array<TreeItem>>) => {
 			</div>
 			<div className={css.body}>
 				<div className={css.left}>
-					<div className={css.element}>
+					<div className={cx(css.element, css[locale])}>
 						<div className={css.circleTop} />
 						{parser(content[3][0].value ?? '')}
 					</div>
-					<div className={css.element}>
+					<div className={cx(css.element, css[locale])}>
 						<div className={css.circleTop} />
 						{parser(content[3][1].value ?? '')}
 					</div>
-					<div className={cx(css.element, css.leftArrow)}>
+					<div className={cx(css.element, css.leftArrow, css[locale])}>
 						<div className={css.circleTop} />
 						<div className={css.circleLeft} />
 						{parser(content[3][2].value ?? '')}
 					</div>
-					<div className={css.element}>
+					<div className={cx(css.element, css[locale])}>
 						<div className={css.circleTop} />
 						{parser(content[3][3].value ?? '')}
 					</div>
-					<div className={css.element}>
+					<div className={cx(css.element, css[locale])}>
 						<div className={css.circleTop} />
 						{parser(content[3][4].value ?? '')}
 					</div>
-					<div className={css.element}>
+					<div className={cx(css.element, css[locale])}>
 						<div className={css.circleTop} />
 						{parser(content[3][5].value ?? '')}
 					</div>
-					<div className={cx(css.element, css.leftArrow)}>
+					<div className={cx(css.element, css.leftArrow, css[locale])}>
 						<div className={css.circleTop} />
 						<div className={css.circleLeft} />
 						{parser(content[3][6].value ?? '')}
 					</div>
-					<div className={css.element}>
+					<div className={cx(css.element, css[locale])}>
 						<div className={css.circleTop} />
 						{parser(content[3][7].value ?? '')}
 					</div>
-					<div className={css.element}>
+					<div className={cx(css.element, css[locale])}>
 						<div className={css.circleTop} />
 						{parser(content[3][8].value ?? '')}
 					</div>
 				</div>
 				<div className={css.right}>
 					<div className={css.rightGroup}>
-						<div className={css.rightElement}>
+						<div className={cx(css.rightElement, css[locale])}>
 							{parser(content[5][1].value ?? '')}
 						</div>
-						<div className={css.rightElement}>
+						<div className={cx(css.rightElement, css[locale])}>
 							{parser(content[5][0].value ?? '')}
 						</div>
 					</div>
 					<div className={css.rightGroup}>
-						<div className={css.rightElement}>
+						<div className={cx(css.rightElement, css[locale])}>
 							{parser(content[4][1].value ?? '')}
 						</div>
-						<div className={css.rightElement}>
+						<div className={cx(css.rightElement, css[locale])}>
 							{parser(content[4][0].value ?? '')}
 						</div>
 					</div>
