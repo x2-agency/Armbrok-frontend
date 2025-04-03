1908cb9726c6b4b9e710135a642e3b61c91fa90b
evgeniy.gilyazetdinov <jhongil3007@gmail.com>
2025-04-04 01:27:40 +0300

    feat: фикс размеров наград

--- argentina-brok/widgets/awards/ui/award-item/index.tsx
+++ argentina-brok/widgets/awards/ui/award-item/index.tsx
@@ -1,6 +1,5 @@
 import cx from 'clsx';
 import parser from 'html-react-parser';
-import { usePathname } from 'next/navigation';
 
 import WreathLeftSVG from '@/public/assets/icons/wreath/left.svg';
 import WreathRightSVG from '@/public/assets/icons/wreath/right.svg';
@@ -9,8 +8,6 @@ import type { Award } from '@/shared/types/global.types';
 import css from './index.module.css';
 
 export const AwardItem = ({ description, startYear, endYear }: Award) => {
-	const pathname = usePathname();
-	const isHyLocal = pathname.startsWith('/hy');
 	return (
 		<article className={css.root}>
 			<WreathLeftSVG className={css.wreath} />
@@ -18,9 +15,7 @@ export const AwardItem = ({ description, startYear, endYear }: Award) => {
 				<h4 className={css.age}>
 					{endYear ? `${startYear}-${endYear}` : startYear}
 				</h4>
-				<p className={cx(css.about, { [css.hyAbout]: isHyLocal })}>
-					{parser(cx(description))}
-				</p>
+				<p className={css.about}>{parser(cx(description))}</p>
 			</div>
 			<WreathRightSVG className={css.wreath} />
 		</article>
