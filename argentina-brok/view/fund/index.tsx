a207598cc5782074b2045f2ba4dbd97e430225b3
evgeniy.gilyazetdinov <jhongil3007@gmail.com>
2025-04-08 00:09:22 +0300

    feat: вставить отзыв

--- argentina-brok/view/fund/index.tsx
+++ argentina-brok/view/fund/index.tsx
@@ -1,19 +1,23 @@
 import type { NextPage } from 'next';
 
 import { BannerSection } from '@/widgets/banner-section/ui';
+import { Review } from '@/widgets/review';
 
 import type { FundPageData } from './types/response';
 
-export const Fund: NextPage<FundPageData> = ({ heroSection }) => {
+export const Fund: NextPage<FundPageData> = ({ heroSection, quoteSection }) => {
 	return (
-		<BannerSection
-			type="profix"
-			banner={{
-				title: heroSection?.title ?? '',
-				description: heroSection?.description ?? '',
-				panel: heroSection?.infoCard,
-				icon: heroSection?.background.url ?? '',
-			}}
-		/>
+		<>
+			<BannerSection
+				type="profix"
+				banner={{
+					title: heroSection?.title ?? '',
+					description: heroSection?.description ?? '',
+					panel: heroSection?.infoCard,
+					icon: heroSection?.background.url ?? '',
+				}}
+			/>
+			<Review quote={quoteSection} />
+		</>
 	);
 };
--- argentina-brok/view/fund/types/response.ts
+++ argentina-brok/view/fund/types/response.ts
@@ -1,10 +1,12 @@
 import type {
 	BaseResponse,
 	ProfixHeroSection,
+	QuoteSection,
 } from '@/shared/types/global.types';
 
 export type FundPageData = {
 	heroSection?: ProfixHeroSection;
+	quoteSection?: QuoteSection;
 };
 
 export type FundPageResponse = BaseResponse<FundPageData>;
