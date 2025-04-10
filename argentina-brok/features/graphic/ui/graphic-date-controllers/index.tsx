ba1c12c8b0abe7d7a3640603921881db5dd09370
evgeniy.gilyazetdinov <jhongil3007@gmail.com>
2025-04-10 23:04:34 +0300

    feat: создать ui-контроллеров

--- argentina-brok/features/graphic/ui/graphic-date-controllers/index.tsx
+++ argentina-brok/features/graphic/ui/graphic-date-controllers/index.tsx
@@ -1,1 +1,38 @@
-export const GraphicDateControllers = () => {};
+'use client';
+import cx from 'clsx';
+import parser from 'html-react-parser';
+
+import { Button } from '@/shared/ui/button';
+
+import css from './index.module.css';
+
+export type GraphicControllerProps = {
+	text: string;
+	date: string;
+	number?: number;
+	setRange: (date: string, number?: number) => void;
+};
+
+export type GraphicDateControllersProps = {
+	buttons: Array<GraphicControllerProps>;
+	className?: string;
+};
+
+export const GraphicDateControllers = ({
+	className,
+	buttons,
+}: GraphicDateControllersProps) => {
+	return (
+		<div className={cx(css.root, className)}>
+			{buttons.map((button, index) => (
+				<Button
+					className={css.button}
+					onClick={() => button.setRange(button.date, button.number)}
+					key={index}
+				>
+					{parser(button.text)}
+				</Button>
+			))}
+		</div>
+	);
+};
