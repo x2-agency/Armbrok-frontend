1b8fdd757cddd998e6369d2777f186181c6bbb64
evgeniy.gilyazetdinov <jhongil3007@gmail.com>
2025-05-06 01:14:06 +0300

    feat: исправить перекраску хэдера

--- argentina-brok/widgets/app-layout/hooks/use-header-color.tsx
+++ argentina-brok/widgets/app-layout/hooks/use-header-color.tsx
@@ -2,7 +2,7 @@ import { usePathname } from 'next/navigation';
 import { useEffect } from 'react';
 
 import { useScrollObserver } from '@/shared/hooks/use-scroll-observer';
-import { EXCLUDED_URLS } from '@/widgets/app-layout/model/header.constants';
+import { INCLUDED_URLS } from '@/widgets/app-layout/model/header.constants';
 
 export const setHeaderColorAttribute = (color: 'dark' | 'white') => {
 	document.documentElement.setAttribute('data-header-top-color', color);
@@ -10,15 +10,14 @@ export const setHeaderColorAttribute = (color: 'dark' | 'white') => {
 
 export const useHeaderColor = () => {
 	const formattedUrl = usePathname().split('/')[2];
-	const isExcluded = EXCLUDED_URLS.includes(formattedUrl);
+	const isIncluded = INCLUDED_URLS.includes(formattedUrl);
 
 	useEffect(() => {
-		setHeaderColorAttribute('white');
-		if (isExcluded) setHeaderColorAttribute('dark');
-	}, [isExcluded]);
+		setHeaderColorAttribute(isIncluded ? 'white' : 'dark');
+	}, [isIncluded]);
 
 	useScrollObserver(isScrolled => {
-		if (isExcluded) return;
+		if (!isIncluded) return;
 		setHeaderColorAttribute(isScrolled ? 'dark' : 'white');
 	});
 };
