fbab170a4ecc611a9255aa7086002d53c52aeb0b
evgeniy.gilyazetdinov <jhongil3007@gmail.com>
2025-05-07 22:57:28 +0300

    feat: изменить обрезку слов

--- argentina-brok/features/graphic/model/graphic.constants.ts
+++ argentina-brok/features/graphic/model/graphic.constants.ts
@@ -5,4 +5,4 @@ export const FILTER_DATA = {
 export const UNIT_PRICE_LABEL = 'Unit price';
 export const NAV_LABEL = 'NAV';
 export const NOT_SELECTED_VALUE = 'Not selected';
-export const MAX_STRING_LENGTH = 32;
+export const MAX_STRING_LENGTH = 18;
