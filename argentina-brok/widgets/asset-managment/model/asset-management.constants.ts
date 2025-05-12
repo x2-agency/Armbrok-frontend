3beb8f5dae415531e6ab49866aab4c050ee4547b
evgeniy.gilyazetdinov <jhongil3007@gmail.com>
2025-05-12 23:08:46 +0300

    feat: заменить руку

---
+++ argentina-brok/widgets/asset-managment/model/asset-management.constants.ts
@@ -0,0 +1,8 @@
+export const ASSET_MANAGEMENT_IMAGES = {
+	mobile: {
+		url: '/assets/images/asset-management/mobile.png',
+	},
+	desktop: {
+		url: '/assets/images/asset-management/pc.png',
+	},
+};
