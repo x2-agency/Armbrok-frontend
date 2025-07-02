e9f14f1a96389a52e8de62f298b3fd588ca2911b
Nikita Slesarev <nikita.slesarev@chulakov.com>
2025-07-02 21:36:03 +0300

    fix: 3 правка

--- argentina-brok/widgets/app-layout/ui/footer/index.tsx
+++ argentina-brok/widgets/app-layout/ui/footer/index.tsx
@@ -3,14 +3,12 @@ import { Container } from '@/shared/ui/container';
 import { Disclaimer } from './disclaimer';
 import css from './index.module.css';
 import { NavigationColumn } from './navigation-column';
-import { SocialLinks } from './social-links';
 
 export const Footer = () => {
 	return (
 		<footer className={css.root}>
 			<Container>
 				<NavigationColumn />
-				<SocialLinks />
 				<Disclaimer />
 			</Container>
 		</footer>
