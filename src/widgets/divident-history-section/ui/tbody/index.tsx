610ba9d7fecfeac57e0929577e0bb53540c66bb4
Nikita Slesarev <nikita.slesarev@chulakov.com>
2025-09-17 21:52:28 +0300

    fix: правки с лума

--- src/widgets/divident-history-section/ui/tbody/index.tsx
+++ src/widgets/divident-history-section/ui/tbody/index.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
 import cx from 'clsx';
 import Link from 'next/link';
 
+import { Button } from '@/shared/ui/button';
 import { formatDate } from '@/widgets/divident-history-section/helpers/format-date';
 import { formatFileExt } from '@/widgets/divident-history-section/helpers/format-file-ext';
 
@@ -24,15 +26,43 @@ export type TBodyProps = {
 };
 
 export const Tbody = ({ list }: TBodyProps) => {
+	const defineIcon = (file?: TbodyType['file']) => {
+		if (file?.ext === '.xlsx') {
+			return '/assets/icon/file/excel.svg';
+		}
+		return '/assets/icon/file/file.svg';
+	};
+
 	return (
 		<tbody className={css.tbody}>
 			{list.map(item => (
 				<tr key={item.id} className={cx(css.row, css.bodyRow)}>
-					<td className={cx(css.tbodyValue, css.leftAlign)}>{formatDate(item.date)}</td>
-					<td className={cx(css.tbodyValue, css.rightAlign)}>{item.financialYear}</td>
+					<td className={cx(css.tbodyValue, css.leftAlign)}>
+						{formatDate(item.date)}
+					</td>
+					<td className={cx(css.tbodyValue, css.rightAlign)}>
+						{item.financialYear}
+					</td>
 
-					<td className={cx(css.tbodyValue, css.rightAlign)}>{item.dividendSize}</td>
-					<td className={cx(css.tbodyValue, css.rightAlign)}>{formatFileExt(item.file)}</td>
+					<td className={cx(css.tbodyValue, css.rightAlign)}>
+						{item.dividendSize}
+					</td>
+					<td className={cx(css.tbodyValue, css.rightAlign)}>
+						<img
+							loading="lazy"
+							src={defineIcon()}
+							className={css.icon}
+							alt="icon"
+						/>{' '}
+						<Button
+							variant="next"
+							iconRotate={270}
+							className={css.button}
+							onClick={() => formatFileExt(item.file)}
+						>
+							Download
+						</Button>
+					</td>
 					<Link
 						href={item?.file?.url ?? '#'}
 						className={css.link}
