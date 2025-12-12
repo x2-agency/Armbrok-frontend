e0c900a937de6ab1d13914aa9101312a7bf0ad1c
PxrpGill <jhongil3007@gmail.com>
2025-12-12 23:42:45 +0300

    feat: add links integration

--- src/widgets/internal-rules/index.tsx
+++ src/widgets/internal-rules/index.tsx
@@ -6,20 +6,22 @@ import { Container } from '@/shared/ui/container';
 import css from './index.module.css';
 import { ButtonPage } from './ui/button-page';
 
+export type InternalLinkType = { text?: string; link?: string };
+
 export type InternalRulesType = {
 	title?: string;
-	text?: Array<{ value: string }>;
+	links?: Array<InternalLinkType>;
 };
 
 export type InternalRulesProps = {
 	internalRules?: InternalRulesType;
 };
 export const InternalRules = ({ internalRules }: InternalRulesProps) => {
-	const { title, text } = internalRules || {};
+	const { title, links } = internalRules || {};
 	return (
 		<Container className={cx(css.root)}>
 			<h3 className={css.title}>{parser(title ?? '')}</h3>
-			<ButtonPage text={text} />
+			<ButtonPage links={links} />
 		</Container>
 	);
 };
--- src/widgets/internal-rules/ui/button-page/index.tsx
+++ src/widgets/internal-rules/ui/button-page/index.tsx
@@ -4,23 +4,27 @@ import cx from 'clsx';
 import parser from 'html-react-parser';
 import Link from 'next/link';
 
+import type { InternalLinkType } from '@/widgets/internal-rules';
+
 import css from './index.module.css';
 
 export type ButtonPageType = {
-	text?: Array<{ value: string; title?: string }>;
+	links?: Array<InternalLinkType>;
 };
 
-export const ButtonPage = ({ text }: ButtonPageType) => {
-	if (!text?.length) return null;
+export const ButtonPage = ({ links }: ButtonPageType) => {
+	if (!links?.length) return null;
 
 	return (
 		<ul className={css.root}>
-			{text.map((item, index) => (
+			{links.map((item, index) => (
 				<li key={index} className={cx(css.listItem, 'p-32')}>
 					<article className={css.wrap}>
-						<p className={css.title}>{parser(item?.title ?? 'title')}</p>
-
-						<Link href={`/regulation/${item.value}`} className={css.link} />
+						<p className={css.title}>{parser(item?.text ?? 'title')}</p>
+						<Link
+							href={`/regulation/${item.link ?? '#'}`}
+							className={css.link}
+						/>
 					</article>
 				</li>
 			))}
