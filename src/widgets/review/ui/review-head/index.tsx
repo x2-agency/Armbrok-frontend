fba168bf36c0a59115eba6862456004fcf36e488
NSLES <n.slesarev29@gmail.com>
2026-04-18 19:48:50 +0300

    fix: правки

--- src/widgets/review/ui/review-head/index.tsx
+++ src/widgets/review/ui/review-head/index.tsx
@@ -7,7 +7,7 @@ import css from './index.module.css';
 export const ReviewHead = ({ title, review }: ReviewHeadProps) => {
 	return (
 		<header className={css.root}>
-			<h2 className={css.title}>{parser(title)}</h2>
+			{title && <h2 className={css.title}>{parser(title)}</h2>}
 			<p className={css.text}>{parser(review)}</p>
 		</header>
 	);
